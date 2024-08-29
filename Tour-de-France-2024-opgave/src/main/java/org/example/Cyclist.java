dd46fd92acded43f4b604c4e4fc90de907c72382
signelystrup <166133407+signelystrup@users.noreply.github.com>
2024-08-29 10:08:16 +0200

    cyclist

--- Tour-de-France-2024-opgave/src/main/java/org/example/Cyclist.java
+++ Tour-de-France-2024-opgave/src/main/java/org/example/Cyclist.java
@@ -1,4 +1,10 @@
 package org.example;
 
 public class Cyclist {
+    private String name;
+    private String team;
+    public Cyclist(String name, String team){
+        this.name = name;
+        this.team = team;
+    }
 }
